82452371362ce1c97b4cedcf0ef70db322ffc383
saifoelloh <saifoelloh@gmail.com>
2019-08-19 16:12:16 +0700

    creating empty method in purchase interface

--- src/main/java/com/example/demo/service/impl/InterfacePurchase.java
+++ src/main/java/com/example/demo/service/impl/InterfacePurchase.java
@@ -1,4 +1,14 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Item;
+import com.example.demo.entity.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface InterfacePurchase {
+    List<Purchase> findAll();
+    Optional<Purchase> findById(Long id);
+    void store(Purchase purchase);
+    void destroy(Long id);
 }
